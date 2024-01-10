d54a1f972de5f098f422928efd306fc04c9198a3
Eric Yun <yunseric@gmail.com>
2024-01-09 23:09:16 -0800

    Add Users collection and configure dotenv

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -1,6 +1,6 @@
-import { AuthCredentialsValidator } from "@/lib/validators/account-credentials-validator";
+import { AuthCredentialsValidator } from "../lib/validators/account-credentials-validator";
 import { router, publicProcedure } from "./trpc"; 
-import { getPayloadClient } from "@/get-payload";
+import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
 
 
@@ -32,4 +32,4 @@ export const authRouter = router({
         }
       })
     }),
-});
+})
