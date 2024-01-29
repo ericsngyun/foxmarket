95ad96fd1dd3730d13c6794285b996391817d928
Eric Yun <yunseric@gmail.com>
2024-01-28 21:19:32 -0800

    Fix bug in login functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
@@ -46,7 +47,7 @@ export default function Home() {
 
           </div>
         </div>
-
+        <ProductReel/>
         {/* TODO: List Products! */}
       </MaxWidthWrapper>
 
