7491507d33eb7b9a1157f56630eefd81c0797749
Eric Yun <yunseric@gmail.com>
2024-02-04 23:55:55 -0800

    Fix breadcrumb link styling

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -10,23 +10,35 @@ interface PageProps {
 const BREADCRUMBS = [
   {id: 1, name: "Home", href: "/"},
   {id:2, name: "Products", href: "/products"},
-  {}
 ]
 
 const Page = ({ params }: PageProps) => {
   return (
-    <MaxWidthWrapper className = 'bg-white'>
-      <div className = 'bg-white'>
-        <div className = 'mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
+    <MaxWidthWrapper className="bg-white">
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           {/* Product Details */}
-          <div className = 'lg:max-w-lg lg:self-end'>
-            <ol className = 'flex items-center space-x-2'>
+          <div className="lg:max-w-lg lg:self-end">
+            <ol className="flex items-center space-x-2">
               {BREADCRUMBS.map((breadcrumb, i) => (
-                <li key = {breadcrumb.href}>
-                  <div className = "flex items-center text-sm">
-                    <Link href = {breadcrumb.href} className = 'font-medium text-sm text-muted-foreground'>
-
+                <li key={breadcrumb.href}>
+                  <div className="flex items-center text-sm">
+                    <Link
+                      href={breadcrumb.href}
+                      className="font-medium text-sm text-muted-foreground hover:text-gray-900"
+                    >
+                      {breadcrumb.name}
                     </Link>
+                    {i !== BREADCRUMBS.length - 1 ? (
+                      <svg
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                        className="ml-2 h-5 w-5 flex-shrink-0 text-gray-300"
+                      >
+                        <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+                      </svg>
+                    ) : null}
                   </div>
                 </li>
               ))}
@@ -35,7 +47,7 @@ const Page = ({ params }: PageProps) => {
         </div>
       </div>
     </MaxWidthWrapper>
-  )
+  );
 };
 
 export default Page
