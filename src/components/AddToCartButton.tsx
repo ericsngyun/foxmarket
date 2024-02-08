0de51b3db1d8b759dc7066a1ae66317a726fb115
Eric Yun <yunseric@gmail.com>
2024-02-07 16:13:27 -0800

    Add AddToCartButton component to product page

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,5 +1,34 @@
+'use client'
+
+import { useEffect, useState } from "react"
+import { Button } from "./ui/button"
+
 const AddToCartButton = () => {
-  
+
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false)
+    }, 200)
+
+    return () => clearTimeout(timeout)
+
+  },[isSuccess])
+
+
+
+  return (
+    <Button
+      onClick={() => {
+        setIsSuccess(true);
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? 'Added!' : 'Add to cart'}
+    </Button>
+  )
 }
 
 export default AddToCartButton
