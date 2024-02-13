784e17429b2bc5eb4186586b7c9df3484f83cf33
Eric Yun <yunseric@gmail.com>
2024-02-12 23:16:04 -0800

    Add checkout functionality and update product creation and update

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { privateProcedure, router } from "./trpc"
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
-import { stripe } from "@/lib/stripe";
+import { stripe } from "../lib/stripe";
 import type Stripe from "stripe";
 
 export const paymentRouter = router({
@@ -40,24 +40,39 @@ export const paymentRouter = router({
 
       const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
 
+      filteredProducts.forEach((product) => {
+        line_items.push({
+          price: product.priceId!,
+          quantity: 1,
+        })
+      })
+
       line_items.push({
-        price: "price_1OjFkhGMPGzx7MQWsJ8nc49C"
+        price: "price_1OjFkhGMPGzx7MQWsJ8nc49C",
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false
+        }
       })
 
       try {
         const stripeSession = await stripe.checkout.sessions.create({
           success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
-          cancel_url=`${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
-          payment_method_types: ["cart", "paypal"],
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card", "paypal", "klarna"],
           mode: "payment",
           metadata: {
             userId: user.id,
             orderId: order.id,
           },
-          line_items: 
+          line_items,
         })
-      } catch(err) {
 
+        return { url: stripeSession.url }
+      } catch(err) {
+        console.log(err)
+
+        return { url: null }
       }
     }),
 });
